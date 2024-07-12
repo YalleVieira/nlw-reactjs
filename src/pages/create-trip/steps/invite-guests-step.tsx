da14b7efb29a01e4532be2753dc2d3462fac2ceb
YalleVieira <yalle.vieira99@gmail.com>
2024-07-12 12:00:11 -0300

    feat: trip details page

--- src/pages/create-trip/steps/invite-guests-step.tsx
+++ src/pages/create-trip/steps/invite-guests-step.tsx
@@ -30,6 +30,8 @@ export function InviteGuestsStep({
         )}
       </button>
 
+      <div className="w-fill h-px bg-zinc-800" />
+
       <button
         onClick={toggleConfirmTripModal}
         className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400"
